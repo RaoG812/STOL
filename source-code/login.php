70ca336907613c9b9e314a127500550dab818fc3
RaoG812 <140965516+RaoG812@users.noreply.github.com>
2024-07-20 17:45:34 +0700

    Update login.php

--- source-code/login.php
+++ source-code/login.php
@@ -1,3 +1,17 @@
+<?php
+// Start the session
+session_start();
+
+
+// When the user is logged in, go to the user page
+if (isset($_SESSION['logged-in']) && $_SESSION['logged-in'] == TRUE) {
+    die(header('Location: user.php'));
+}
+
+
+// Place username of your bot here
+define('BOT_USERNAME', 'XXXXXXXXXXXX');
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 
@@ -14,7 +28,7 @@
     <h1>Sign Up & Login With Telegram</h1>
     <div class="middle-center">
         <h1>Hello, Anonymous!</h1>
-        <script async src="https://telegram.org/js/telegram-widget.js" data-telegram-login="<?= stol_food_bot ?>" data-size="large" data-auth-url="auth.php"></script>
+        <script async src="https://telegram.org/js/telegram-widget.js" data-telegram-login="<?= BOT_USERNAME ?>" data-size="large" data-auth-url="auth.php"></script>
     </div>
 </body>
 
