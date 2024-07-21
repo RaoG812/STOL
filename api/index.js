1ffbb9ecea9ea84877870d2a5bbbb84c3cfb7410
RaoG812 <140965516+RaoG812@users.noreply.github.com>
2024-07-21 18:38:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const auth = require('./auth');
+const authRoutes = require('./auth'); // Import auth.js
 
 const app = express();
-
-app.use(bodyParser.json());
-
-app.post('/auth', auth);
-
 const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/auth', authRoutes); // Use routes defined in auth.js
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
